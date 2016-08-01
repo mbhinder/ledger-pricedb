1eb68255093e8a8fa4f97da93d4abcea339718db
Preet Bhinder <preet@apcera.com>
2016-07-31 18:06:34 -0700

    Initial working commit
    Working commit, can download a list of tickers to local pricedb file.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # ledger-pricedb
-Elisp code to fetch stock tickers in ledger price db format
+
+Simple emacs package to generate and update a pricedb
+file for ledger-cli (http://ledger-cli.org)
+
+Stock tickers can be added from your ledger file and current prices
+downloaded automatically using the Yahoo! finance API.
+
+# Basic use and configuration
+```elisp
+
+(load-file "~/ledger-pricedb/ledger-pricedb.el")
+(set 'ledger-pricedb--stocks '("VGTSX" "VFIFX" "VBTLX" "VTIAX" "VTSAX" "VTSMX" "NFLX" "VSMAX"))
+(set 'ledger-pricedb--pricedb "~/ledger/.pricedb")
+
+(global-set-key (kbd "C-c s") (lambda () (interactive) (ledger-pricedb-save-pricedb)))
+
+```
+
+# Output in pricedb:
+
+```
+P 2016/07/31 18:00:05 VGTSX $14.76
+P 2016/07/31 18:00:05 VFIFX $30.19
+P 2016/07/31 18:00:05 VBTLX $11.11
+P 2016/07/31 18:00:05 VTIAX $24.70
+P 2016/07/31 18:00:05 VTSAX $54.13
+P 2016/07/31 18:00:05 VTSMX $54.11
+P 2016/07/31 18:00:05 NFLX $91.65
+P 2016/07/31 18:00:05 VSMAX $58.03
+```
---
+++ ledger-pricedb.el
@@ -0,0 +1,40 @@
+;;; ledger-pricedb.el --- Simple extension to generate a pricedb file for ledger
+
+;;; Commentary:
+;; ledger-pricedb--stocks configures the list of ticker to get
+;; ledger-pricedb--pricedb configures the location of the pricedb file
+;; ledger-pricedb-get-prices fetches the tickers using the Yahoo API
+;; ledger-pricedb-generate-pricedb generates a list of strings compatible with the format
+;; ledger-pricedb-save-pricedb saves the file to disk
+
+;;; Code:
+(defvar ledger-pricedb--yahoo_uri nil)
+(defvar ledger-pricedb--stocks nil)
+(defvar ledger-pricedb--pricedb nil)
+
+(set 'ledger-pricedb--yahoo_uri "http://download.finance.yahoo.com/d/quotes.csv?s=")
+
+(defun ledger-pricedb-get-prices (uri tickers)
+  "Get prices from Yahoo using URI for the http uri for the tickers specified in TICKERS."
+  (split-string
+   (with-current-buffer (url-retrieve-synchronously
+                         (concat uri (mapconcat 'identity tickers "+") "&f=p"))
+     (goto-char (point-min))
+     (re-search-forward "^\n")
+     (delete-region (point) (point-min))
+     (buffer-string)) "\n"))
+
+(defun ledger-pricedb-generate-pricedb (prices)
+  "Take a list of PRICES usually obtained by ledger-pricedb-get-prices and convert them into pricedb format."
+  (cl-mapcar (lambda (x y) (format "P %s %s $%s" (format-time-string "%Y/%m/%d %H:%M:%S") y x))
+             prices
+             ledger-pricedb--stocks))
+
+(defun ledger-pricedb-save-pricedb ()
+  "Save the prices downloaded by generate-pricedb to the pricedb file."
+  (write-region "\n" nil  ledger-pricedb--pricedb 'append)
+  (write-region (mapconcat 'identity (ledger-pricedb-generate-pricedb (ledger-pricedb-get-prices ledger-pricedb--yahoo_uri ledger-pricedb--stocks)) "\n") nil  ledger-pricedb--pricedb 'append)
+  (write-region "\n" nil  ledger-pricedb--pricedb 'append))
+
+(provide 'ledger-pricedb)
+;;; ledger-pricedb.el ends here
